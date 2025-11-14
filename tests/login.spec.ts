5925f6987977e23efc60a868561d38d409e00850
Mateusz <tokarski.mateusz5@gmail.com>
2025-11-14 12:35:54 +0100

    test: add authentication validation tests

---
+++ tests/login.spec.ts
@@ -0,0 +1,32 @@
+import { expect, test } from '../src/api/merge.fixture';
+
+test.describe('Admin', () => {
+  test('should display admin greeting and logout button when authenticated', async ({
+    loggedAdminPage,
+  }) => {
+    await expect(loggedAdminPage.getByText('Hi, Admin')).toBeVisible();
+    await expect(
+      loggedAdminPage.getByRole('button', { name: 'Logout' }),
+    ).toBeVisible();
+  });
+});
+
+test.describe('User', () => {
+  test('should display user greeting and logout button when authenticated', async ({
+    loggedUserPage,
+  }) => {
+    await expect(loggedUserPage.getByText('Hi, John Doe')).toBeVisible();
+    await expect(
+      loggedUserPage.getByRole('button', { name: 'Logout' }),
+    ).toBeVisible();
+  });
+
+  test('should not display user greeting and logout button when not authenticated', async ({
+    page,
+  }) => {
+    await page.goto('/');
+    await expect(page.getByText('Login')).toBeVisible();
+    await expect(page.getByText('Hi, John Doe')).toBeHidden();
+    await expect(page.getByRole('button', { name: 'Logout' })).toBeHidden();
+  });
+});
