8ee650ed486ac28003bf55fed6af34b2c5f5f151
Mateusz <tokarski.mateusz5@gmail.com>
2025-11-21 14:16:45 +0100

    feat: add more locators to simple home page

--- src/ui/pages/home.page.ts
+++ src/ui/pages/home.page.ts
@@ -3,10 +3,16 @@ import { Locator, Page } from '@playwright/test';
 export class HomePage {
   readonly page: Page;
   readonly navbar: Locator;
+  readonly userGreeting: Locator;
+  readonly loginLink: Locator;
+  readonly logoutButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.navbar = page.getByRole('navigation');
+    this.userGreeting = page.locator('span[class="nav-link"]');
+    this.loginLink = page.getByRole('link', { name: 'Login' });
+    this.logoutButton = page.getByRole('button', { name: 'Logout' });
   }
 
   async goto(): Promise<void> {
