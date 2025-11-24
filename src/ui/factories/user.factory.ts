f6d13c6b33b477df7916482ffac73687bd0d5ecb
Mateusz <tokarski.mateusz5@gmail.com>
2025-11-24 13:00:18 +0100

    feat: create factory for user

---
+++ src/ui/factories/user.factory.ts
@@ -0,0 +1,14 @@
+import { SignUpModel } from '../models/sign-up.model';
+import { faker } from '@faker-js/faker/locale/en';
+
+export function prepareRandomUser(): SignUpModel {
+  const name = faker.person.firstName().replace(/[^A-Za-z]/g, '');
+  const password = faker.internet.password({ length: 8 });
+
+  return {
+    name: name,
+    email: faker.internet.email({ firstName: name }),
+    password: password,
+    confirmPassword: password,
+  };
+}
