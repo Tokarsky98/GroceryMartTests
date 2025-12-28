d08eb9a53a039f27785b8df4b2bca9994a4954a7
Mateusz <tokarski.mateusz5@gmail.com>
2025-12-28 10:55:28 +0100

    refactor: adjust merges to current state

--- src/merge.fixture.ts
+++ src/merge.fixture.ts
@@ -1,7 +1,7 @@
-import { authTest } from '@_api/fixtures/auth.fixture';
+import { requestObjectTest } from '@_api/fixtures/request-object.fixture';
 import { authenticatedPageTest } from '@_ui/fixtures/authenticated-page.fixture';
 import { mergeTests } from '@playwright/test';
 
-export const test = mergeTests(authTest, authenticatedPageTest);
+export const test = mergeTests(authenticatedPageTest, requestObjectTest);
 
 export { expect } from '@playwright/test';
