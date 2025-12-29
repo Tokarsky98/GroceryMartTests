b1b0da76301f5175856447a2a77720d052ab73bc
Mateusz <tokarski.mateusz5@gmail.com>
2025-12-29 14:47:11 +0100

    refactor: use LoginModel in auth factory

--- src/api/factories/auth-header.factory.ts
+++ src/api/factories/auth-header.factory.ts
@@ -1,14 +1,14 @@
 import { HeadersModel } from '@_api/models/headers.model';
+import { LoginModel } from '@_api/models/login.model';
 import { LoginRequest } from '@_api/requests/login.request';
 import { APIRequestContext, expect } from '@playwright/test';
 
 export async function getAuthHeader(
   request: APIRequestContext,
-  email: string,
-  password: string,
+  loginData: LoginModel,
 ): Promise<HeadersModel> {
   const loginRequest = new LoginRequest(request);
-  const loginResponse = await loginRequest.post({ email, password });
+  const loginResponse = await loginRequest.post(loginData);
   await expect(loginResponse).toBeOK();
 
   const loginResponseJson = await loginResponse.json();
