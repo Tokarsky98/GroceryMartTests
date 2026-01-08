6bb88fec7a791dcccf634ae8c4e47ee28b234c7f
Mateusz <tokarski.mateusz5@gmail.com>
2026-01-08 14:57:52 +0100

    refactor: change file name to user-context.fixture.ts

--- src/merge.fixture.ts
+++ src/merge.fixture.ts
@@ -1,5 +1,5 @@
 import { dataFixtureTest } from '@_api/fixtures/data.fixture';
-import { userContextTest } from '@_ui/fixtures/authenticated-page.fixture';
+import { userContextTest } from '@_src/ui/fixtures/user-context.fixture';
 import { mergeTests } from '@playwright/test';
 
 export const test = mergeTests(userContextTest, dataFixtureTest);
