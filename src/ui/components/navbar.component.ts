5cd0e4569bcb27e4cffd232579a25ad9dfb4793e
Mateusz <tokarski.mateusz5@gmail.com>
2026-01-13 12:55:07 +0100

    refactor: remove redundant method and adjust naming of cart locators

--- src/ui/components/navbar.component.ts
+++ src/ui/components/navbar.component.ts
@@ -5,7 +5,8 @@ export class NavbarComponent {
   readonly root: Locator;
   readonly homeLink: Locator;
   readonly adminLink: Locator;
-  readonly cartBadge: Locator;
+  readonly cartIcon: Locator;
+  readonly cartIconBadge: Locator;
   readonly userGreeting: Locator;
   readonly loginLink: Locator;
   readonly logoutButton: Locator;
@@ -14,19 +15,15 @@ export class NavbarComponent {
     this.root = root;
     this.homeLink = root.getByRole('link', { name: 'Home' });
     this.adminLink = root.getByRole('link', { name: 'Admin' });
-    this.cartBadge = root.locator('.cart-badge');
+    this.cartIcon = root.locator('.cart-badge');
+    this.cartIconBadge = this.cartIcon.locator('.badge');
     this.userGreeting = root.locator('span[class="nav-link"]');
     this.loginLink = root.getByRole('link', { name: 'Login' });
     this.logoutButton = root.getByRole('button', { name: 'Logout' });
   }
 
   async openCart(): Promise<CartComponent> {
-    await this.cartBadge.click();
+    await this.cartIcon.click();
     return new CartComponent(this.root.locator('div[class="cart-dropdown"]'));
-  }
-
-  async getCartBadgeCount(): Promise<string> {
-    const badgeText = await this.cartBadge.locator('.badge').innerText();
-    return badgeText;
   }
 }
